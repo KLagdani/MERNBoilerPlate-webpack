14ee6f447476f6edcc61008deb54c433611484a7
Kaoutar <lagdani.kaoutar@gmail.com>
2020-03-25 11:34:28 +0100

    UPD webpack config for prod

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,34 +1,60 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-    publicPath: "/"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
+module.exports = env => {
+  const isProduction = env === "production";
 
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    historyApiFallback: {
-      disableDotRule: true
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+      publicPath: "/"
     },
-    contentBase: path.resolve(__dirname, "public"),
-    proxy: {
-      "/api": "http://localhost:5000/"
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+
+        {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "styles.css"
+      })
+    ],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      historyApiFallback: {
+        disableDotRule: true
+      },
+      contentBase: path.resolve(__dirname, "public"),
+      proxy: {
+        "/api": "http://localhost:5000/"
+      }
     }
-  }
+  };
 };
